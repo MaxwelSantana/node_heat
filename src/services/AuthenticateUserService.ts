e2d50b47b1fc6ec5f9b64f353a140b13084b621c
Maxwel <maxwelsant96@icloud.com>
2021-10-18 20:21:10 -0300

    get user info with git token

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,20 +1,41 @@
 import axios from 'axios';
 
+interface IAccessTokenResponse {
+  access_token: string;
+}
+interface IUserResponse {
+  avatar_url: string;
+  login: string;
+  id: number;
+  name: string;
+}
 class AuthenticateUserService {
   async execute(code: string) {
     const url = 'https://github.com/login/oauth/access_token';
 
-    const response = await axios.post(url, null, {
-      params: {
-        client_id: process.env.GITHUB_CLIENT_ID,
-        client_secret: process.env.GITHUB_CLIENT_SECRET,
-        code,
+    const { data: accessTokenReponse } = await axios.post<IAccessTokenResponse>(
+      url,
+      null,
+      {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: 'application/json',
+        },
       },
-      headers: {
-        Accept: 'application/json',
-      },
-    });
+    );
 
+    const response = await axios.get<IUserResponse>(
+      'https://api.github.com/user',
+      {
+        headers: {
+          authorization: `Bearer ${accessTokenReponse.access_token}`,
+        },
+      },
+    );
     return response.data;
   }
 }
