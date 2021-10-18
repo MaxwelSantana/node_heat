cf8e0769290861301ea1d5ca3d522ff52d46afea
Maxwel <maxwelsant96@icloud.com>
2021-10-18 20:57:23 -0300

    add createMessageService

--- src/services/CreateMessageService.ts
+++ src/services/CreateMessageService.ts
@@ -1,5 +1,19 @@
+import prismaCliente from '../prisma';
+
 class CreateMessageService {
-  async execute(text: string, user_id: string) {}
+  async execute(text: string, user_id: string) {
+    const message = await prismaCliente.message.create({
+      data: {
+        text,
+        user_id,
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    return message;
+  }
 }
 
 export { CreateMessageService };
