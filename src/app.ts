c988e84a826f6675a2f0328c312ba3bed7688490
Maxwel <maxwelsant96@icloud.com>
2021-10-18 20:11:10 -0300

    get access token from github

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,21 @@
+import 'dotenv/config';
 import express from 'express';
+import { router } from './routes';
 
 const app = express();
 
-app.listen(3000, () => console.log(`Running on port ${3000}`));
+app.use(express.json());
+app.use(router);
+
+app.get('/github', (req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`,
+  );
+});
+
+app.get('/signin/callback', (req, res) => {
+  const { code } = req.query;
+  return res.json(code);
+});
+const port = 4000;
+app.listen(port, () => console.log(`Running on port ${port}`));
