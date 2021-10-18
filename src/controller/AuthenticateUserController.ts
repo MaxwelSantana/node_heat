582c515d50103ed9b0b2f9c40c4db91273836d2e
Maxwel <maxwelsant96@icloud.com>
2021-10-18 20:47:23 -0300

    generate token and save user in db if not exists

--- src/controller/AuthenticateUserController.ts
+++ src/controller/AuthenticateUserController.ts
@@ -1,12 +1,16 @@
-import { Request, Response } from 'express';
+import { Request, response, Response } from 'express';
 import { AuthenticateUserService } from '../services/AuthenticateUserService';
 
 class AuthenticateUserController {
   async handle(req: Request, res: Response) {
     const { code } = req.body;
     const service = new AuthenticateUserService();
-    const result = await service.execute(code);
-    res.json(result);
+    try {
+      const result = await service.execute(code);
+      res.json(result);
+    } catch (err) {
+      return res.status(401).json({ error: err.message });
+    }
   }
 }
 
