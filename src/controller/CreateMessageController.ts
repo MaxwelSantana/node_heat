1b1fc9ea6cd78ad53ed94892f4ef17d5e50f2ee8
Maxwel <maxwelsant96@icloud.com>
2021-10-18 21:21:25 -0300

    CreateMessagecontroller

--- src/controller/CreateMessageController.ts
+++ src/controller/CreateMessageController.ts
@@ -1,12 +1,16 @@
 import { Request, response, Response } from 'express';
 import { AuthenticateUserService } from '../services/AuthenticateUserService';
+import { CreateMessageService } from '../services/CreateMessageService';
 
 class CreateMessageController {
   async handle(req: Request, res: Response) {
-    const {message} = req.body;
+    const { message } = req.body;
+    const { user_id } = req;
 
-    
+    const service = new CreateMessageService();
+    const result = await service.execute(message, user_id);
 
+    return res.json(result);
   }
 }
 
