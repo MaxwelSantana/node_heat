34bbd50f14a7c81b3326c67b99e5fac10471c912
Maxwel <maxwelsant96@icloud.com>
2021-10-18 22:02:28 -0300

    profile me route

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { AuthenticateUserController } from './controller/AuthenticateUserController';
 import { CreateMessageController } from './controller/CreateMessageController';
 import { GetLast3MessagesController } from './controller/GetLast3MessagesController';
+import { ProfileUserController } from './controller/ProfileUserController';
 import { ensureAuthenticated } from './middleware/ensureAuthenticated';
 
 const router = Router();
@@ -15,5 +16,6 @@ router.post(
 );
 
 router.get('/messages/last3', new GetLast3MessagesController().handle);
+router.get('/me', ensureAuthenticated, new ProfileUserController().handle);
 
 export { router };
